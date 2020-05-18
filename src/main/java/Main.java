46d8f6435d4532878afaea3dfe8212d30b530efc
vladmir.pavluchinskiy <vladmir.pavluchinskiy@gmail.com>
2020-05-18 22:26:08 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        MonteKarlo monteKarlo = new MonteKarlo();
-        monteKarlo.evaluatePI(3);
+        System.out.println("Hi");
     }
 }
