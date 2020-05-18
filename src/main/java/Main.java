d86fb3f2d41bd5242135f5b467e11f681ac32af2
vladmir.pavluchinskiy <vladmir.pavluchinskiy@gmail.com>
2020-05-18 22:33:43 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 public class Main {
     public static void main(String[] args) {
         MonteKarlo monteKarlo = new MonteKarlo();
--- src/main/java/MonteKarlo.java
+++ src/main/java/MonteKarlo.java
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
